a32364208deafcddeba3751b0a980664dc9e0249
machida23 <machida23@up.edu>
2021-04-08 16:38:03 -0700

    easy player/ human player

--- app/src/main/java/com/example/chessgameframework/game/GameFramework/chessPlayers/ChessComputerPlayerEasy.java
+++ app/src/main/java/com/example/chessgameframework/game/GameFramework/chessPlayers/ChessComputerPlayerEasy.java
@@ -45,24 +45,25 @@ public class ChessComputerPlayerEasy extends GameComputerPlayer
 
         // move a piece at random
         Piece randomPiece = null;
-        int randomX = -1, randomY = -1;
+        int randomX = -1;
+        int randomY = -1;
 
         while(randomPiece == null) {
 
             // select the random piece
             while (randomPiece == null) {
-                randomX = (int) (Math.random() * 8);
-                randomY = (int) (Math.random() * 8);
-                if (gameState.currPlayer == 0 && randomPiece.isBlack() == false) {
+                randomX = (int) (Math.random() * 8 + 1);
+                randomY = (int) (Math.random() * 8 + 1);
+                if (gameState.currPlayer == 0 && !randomPiece.isBlack()) {
                     randomPiece = gameState.getPiece(randomX, randomY);
-                } else if (gameState.currPlayer == 1 && randomPiece.isBlack() == true) {
+                } else if (gameState.currPlayer == 1 && randomPiece.isBlack()) {
                     randomPiece = gameState.getPiece(randomX, randomY);
                 }
             }
 
             // find and make a move for piece, otherwise find another piece to move
-            for (int moveX = 0; moveX < 8; moveX++) {
-                for (int moveY = 0; moveY < 8; moveY++) {
+            for (int moveX = 1; moveX <= 8; moveX++) {
+                for (int moveY = 1; moveY <= 8; moveY++) {
 
                     if (randomPiece.canMove(randomX, randomY, moveX, moveY)) {
                         sleep(1);
