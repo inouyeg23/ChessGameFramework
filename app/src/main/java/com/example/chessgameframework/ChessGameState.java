7ed6eaf0e8ec45f09be70d7d76cc6b7bb927de18
ingler24 <ingler24@up.edu>
2021-04-09 14:52:10 -0700

    Worked on check methods in the gamestate

--- app/src/main/java/com/example/chessgameframework/ChessGameState.java
+++ app/src/main/java/com/example/chessgameframework/ChessGameState.java
@@ -74,8 +74,8 @@ public class ChessGameState extends GameState implements Serializable {
     public ChessGameState(){
         //initialize an empty board
         board = new Piece[8][8];
-        for (int i = 1; i <= 8; i++) {
-            for (int j = 1; j <= 8; j++) {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
                 board[i][j] = null;
             }
         }
